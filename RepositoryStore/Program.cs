cb5fa5ce6487e5ebf639b34a025e082b076c20a3
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2026-03-09 17:02:02 -0300

    feat: atualizacoes do curso

--- RepositoryStore/Program.cs
+++ RepositoryStore/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using RepositoryStore.Data;
+using RepositoryStore.Models;
+using RepositoryStore.Repositories;
+using RepositoryStore.Repositories.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     x.UseSqlServer(connectionString);
 });
 // Add services to the container.
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,5 +33,24 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/v1/products", async (CancellationToken token, IProductRepository repository)
+    => Results.Ok(await repository.GetAllAsync(0, 5, token)));
+
+app.MapGet("/v1/products/{id}", async (CancellationToken token, IProductRepository repository, int id)
+    => Results.Ok(await repository.GetByIdAsync(id, token)));
+
+app.MapPost("/v1/products/{id}", async (CancellationToken token, IProductRepository repository, Product product)
+    => Results.Ok(await repository.CreateAsync(product, token)));
+
+app.MapPut("/v1/products", async (CancellationToken token, IProductRepository repository, Product product)
+    => Results.Ok(await repository.UpdateAsync(product, token)));
+
+app.MapDelete("/v1/products/{id}", async (CancellationToken token, IProductRepository repository, int id) =>
+{
+    var product = await repository.GetByIdAsync(id);
+    return product == null ? Results.NotFound()
+        : Results.Ok(await repository.DeleteAsync(product, token));
+});
 
 app.Run();
---
+++ RepositoryStore/Repositories/ProductRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryStore.Data;
+using RepositoryStore.Models;
+using RepositoryStore.Repositories.Abstractions;
+
+namespace RepositoryStore.Repositories;
+
+public class ProductRepository(AppDbContext context) : IProductRepository
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        await _context.Products.AddAsync(product, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return product;
+    }
+
+    public async Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return product;
+    }
+
+    public async Task<Product> DeleteAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return product;
+    }
+
+    public async Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await _context.Products
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+    }
+
+    public async Task<List<Product>?> GetAllAsync(
+        int skip = 0, int take = 25, CancellationToken cancellationToken = default)
+    {
+        return await _context.Products
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync(cancellationToken);
+    }
+}
+
+public class FakeProductRepository : IProductRepository
+{
+    public Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> DeleteAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Product>?> GetAllAsync(int skip = 0, int take = 25, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
